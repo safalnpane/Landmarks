d616712f3f3d33de903b5a1895bbc34a00a6a03c
Safal Neupane <safal.npane@gmail.com>
2022-10-11 09:45:16 +0545

    Initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Safal Neupane on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
